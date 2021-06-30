8301c0ad0ee7a297e0951f600527cb776486ceb5
Lucas Matos <lucas@bertonisolutions.com>
2021-06-30 08:46:50 -0600

    updated

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ const App = () => {
         className={
           isDarktheme
             ? "neo-dark neo-global-colors"
-            : "neo-dynamic neo-global-colors"
+            : "neo-light neo-global-colors"
         }
       >
         <NeoNavBar>
